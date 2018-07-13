1a1dc367d0a47a0cabef07e07a6614730d15e474
shaikhzain007 <Zain.Shaikh@McKesson.com>
2018-07-13 10:18:20 -0400

    vector construtor updated with norm

--- src/vector.py
+++ src/vector.py
@@ -1,3 +1,8 @@
 class Vector:
     def __init__(self,nums):
         self.nums = nums
+        self.dims = len(nums)
+        self.norm = self._length()
+
+    def _length(self):
+        return sum([c**2 for c in self.nums])**0.5
